b6c7b3d38cc8a72523fc96f5a8d003114ad403f8
mahad446 <mahadmubashir43@gmail.com>
2025-03-15 15:16:56 -0700

    activity#1

---
+++ activity#1.py
@@ -0,0 +1,8 @@
+i = 10       #initialise
+while i > 0: #iterate loop
+    i = i -1
+    if i == 5:   #condition 1
+        continue  #continue statement
+    #display result
+    print('/ncurrent variabel value :', i)
+print("/ngood bye!")
